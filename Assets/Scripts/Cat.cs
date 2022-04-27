d65bd21a95da13a0a8581b00bfbf6d1bce6113d2
mfischhaber <4630642+mfischhaber@users.noreply.github.com>
2022-04-27 15:06:47 +0200

    Add basic functionality with inheritance, polymorpthism, encapsulation, and abstraction (#1)

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// INHERITANCE
+public class Cat : Animal
+{
+    private bool jumpForward;
+    
+    // POLYMORPHISM
+    public override void Jump()
+    {
+        float amountForward = jumpForward ? 1 : -1;
+        Vector3 forwardAndUp = new Vector3(0, 1 * jumpForce, amountForward * 75);
+        
+        SelfRigidbody.AddForce(forwardAndUp);
+
+        jumpForward = !jumpForward;
+    }
+}
---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// INHERITANCE
+public class Dog : Animal
+{
+    private bool jumpBackward;
+    
+    // POLYMORPHISM
+    public override void Jump()
+    {
+        float amountForward = jumpBackward ? 2 : -2;
+        Vector3 onlyForward = new Vector3(0, 0, amountForward * 125);
+
+        SelfRigidbody.AddForce(onlyForward);
+        
+        jumpBackward = !jumpBackward;
+    }
+}
